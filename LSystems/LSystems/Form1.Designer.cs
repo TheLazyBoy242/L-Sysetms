c9b8a2ce3ae3ce560e7710c7c267547163210ef6
TheLazyBoy242 <danieljbennys@gmail.com>
2017-05-05 08:01:34 +0800

    Add files via upload

---
+++ LSystems/LSystems/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace LSystems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.test_out_lbl = new System.Windows.Forms.Label();
+            this.Run_btn = new System.Windows.Forms.Button();
+            this.Clear_btn = new System.Windows.Forms.Button();
+            this.posang_txtb = new System.Windows.Forms.TextBox();
+            this.posang_lbl = new System.Windows.Forms.Label();
+            this.negang_lbl = new System.Windows.Forms.Label();
+            this.negang_txtb = new System.Windows.Forms.TextBox();
+            this.depth_lbl = new System.Windows.Forms.Label();
+            this.Depth_bar = new System.Windows.Forms.TrackBar();
+            this.Lenght_lbl = new System.Windows.Forms.Label();
+            this.Length_txtb = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Depth_bar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // test_out_lbl
+            // 
+            this.test_out_lbl.AutoSize = true;
+            this.test_out_lbl.Location = new System.Drawing.Point(290, 14);
+            this.test_out_lbl.Name = "test_out_lbl";
+            this.test_out_lbl.Size = new System.Drawing.Size(0, 13);
+            this.test_out_lbl.TabIndex = 0;
+            // 
+            // Run_btn
+            // 
+            this.Run_btn.Location = new System.Drawing.Point(12, 14);
+            this.Run_btn.Name = "Run_btn";
+            this.Run_btn.Size = new System.Drawing.Size(75, 23);
+            this.Run_btn.TabIndex = 1;
+            this.Run_btn.Text = "Run";
+            this.Run_btn.UseVisualStyleBackColor = true;
+            this.Run_btn.Click += new System.EventHandler(this.Run_btn_Click);
+            // 
+            // Clear_btn
+            // 
+            this.Clear_btn.Location = new System.Drawing.Point(12, 43);
+            this.Clear_btn.Name = "Clear_btn";
+            this.Clear_btn.Size = new System.Drawing.Size(75, 23);
+            this.Clear_btn.TabIndex = 2;
+            this.Clear_btn.Text = "Clear";
+            this.Clear_btn.UseVisualStyleBackColor = true;
+            this.Clear_btn.Click += new System.EventHandler(this.Clear_btn_Click);
+            // 
+            // posang_txtb
+            // 
+            this.posang_txtb.Location = new System.Drawing.Point(12, 89);
+            this.posang_txtb.Name = "posang_txtb";
+            this.posang_txtb.Size = new System.Drawing.Size(75, 20);
+            this.posang_txtb.TabIndex = 3;
+            this.posang_txtb.TextChanged += new System.EventHandler(this.posang_txtb_TextChanged);
+            // 
+            // posang_lbl
+            // 
+            this.posang_lbl.AutoSize = true;
+            this.posang_lbl.Location = new System.Drawing.Point(6, 73);
+            this.posang_lbl.Name = "posang_lbl";
+            this.posang_lbl.Size = new System.Drawing.Size(81, 13);
+            this.posang_lbl.TabIndex = 4;
+            this.posang_lbl.Text = "Posetive Angle:";
+            // 
+            // negang_lbl
+            // 
+            this.negang_lbl.AutoSize = true;
+            this.negang_lbl.Location = new System.Drawing.Point(4, 112);
+            this.negang_lbl.Name = "negang_lbl";
+            this.negang_lbl.Size = new System.Drawing.Size(83, 13);
+            this.negang_lbl.TabIndex = 5;
+            this.negang_lbl.Text = "Negative Angle:";
+            // 
+            // negang_txtb
+            // 
+            this.negang_txtb.Location = new System.Drawing.Point(12, 128);
+            this.negang_txtb.Name = "negang_txtb";
+            this.negang_txtb.Size = new System.Drawing.Size(75, 20);
+            this.negang_txtb.TabIndex = 6;
+            this.negang_txtb.TextChanged += new System.EventHandler(this.negang_txtb_TextChanged);
+            // 
+            // depth_lbl
+            // 
+            this.depth_lbl.AutoSize = true;
+            this.depth_lbl.Location = new System.Drawing.Point(33, 151);
+            this.depth_lbl.Name = "depth_lbl";
+            this.depth_lbl.Size = new System.Drawing.Size(54, 13);
+            this.depth_lbl.TabIndex = 7;
+            this.depth_lbl.Text = "Depth = 6";
+            // 
+            // Depth_bar
+            // 
+            this.Depth_bar.Location = new System.Drawing.Point(7, 167);
+            this.Depth_bar.Minimum = 1;
+            this.Depth_bar.Name = "Depth_bar";
+            this.Depth_bar.Size = new System.Drawing.Size(80, 45);
+            this.Depth_bar.TabIndex = 8;
+            this.Depth_bar.Value = 6;
+            this.Depth_bar.Scroll += new System.EventHandler(this.Depth_bar_Scroll);
+            // 
+            // Lenght_lbl
+            // 
+            this.Lenght_lbl.AutoSize = true;
+            this.Lenght_lbl.Location = new System.Drawing.Point(40, 199);
+            this.Lenght_lbl.Name = "Lenght_lbl";
+            this.Lenght_lbl.Size = new System.Drawing.Size(43, 13);
+            this.Lenght_lbl.TabIndex = 9;
+            this.Lenght_lbl.Text = "Length:";
+            // 
+            // Length_txtb
+            // 
+            this.Length_txtb.Location = new System.Drawing.Point(12, 218);
+            this.Length_txtb.Name = "Length_txtb";
+            this.Length_txtb.Size = new System.Drawing.Size(75, 20);
+            this.Length_txtb.TabIndex = 10;
+            this.Length_txtb.Text = "5";
+            this.Length_txtb.TextChanged += new System.EventHandler(this.Length_txtb_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 447);
+            this.Controls.Add(this.Length_txtb);
+            this.Controls.Add(this.Lenght_lbl);
+            this.Controls.Add(this.Depth_bar);
+            this.Controls.Add(this.depth_lbl);
+            this.Controls.Add(this.negang_txtb);
+            this.Controls.Add(this.negang_lbl);
+            this.Controls.Add(this.posang_lbl);
+            this.Controls.Add(this.posang_txtb);
+            this.Controls.Add(this.Clear_btn);
+            this.Controls.Add(this.Run_btn);
+            this.Controls.Add(this.test_out_lbl);
+            this.Name = "Form1";
+            this.Text = "L System Tree";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Depth_bar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label test_out_lbl;
+        private System.Windows.Forms.Button Run_btn;
+        private System.Windows.Forms.Button Clear_btn;
+        private System.Windows.Forms.TextBox posang_txtb;
+        private System.Windows.Forms.Label posang_lbl;
+        private System.Windows.Forms.Label negang_lbl;
+        private System.Windows.Forms.TextBox negang_txtb;
+        private System.Windows.Forms.Label depth_lbl;
+        private System.Windows.Forms.TrackBar Depth_bar;
+        private System.Windows.Forms.Label Lenght_lbl;
+        private System.Windows.Forms.TextBox Length_txtb;
+    }
+}
+
---
+++ LSystems/LSystems/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nakov.TurtleGraphics;
+
+namespace LSystems
+{
+    public partial class Form1 : Form
+    {
+        string posang = "";
+        string negang = "";
+        int length = 5;
+        int depth = 6;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            test_out_lbl.Visible = false;
+            this.BackColor = Color.White;
+        }
+
+        private void Run_btn_Click(object sender, EventArgs e)
+        {
+            int pos_angle;
+            int neg_angle;
+            Random rand = new Random();
+
+            //randomizes the angles for generation if needed
+            if (posang != "") { pos_angle = Convert.ToInt16(posang); }
+            else { pos_angle = rand.Next(5, 35); }
+
+            if (negang != "") { neg_angle = -Convert.ToInt16(negang); }
+            else { neg_angle = -rand.Next(5, 35); }
+
+
+            string this_line = "";
+
+
+            // sets up the turtle graphics tool with the right settings
+            
+            Turtle.PenUp();
+            Turtle.PenColor = Color.Brown;
+            Turtle.ShowTurtle = false;
+            Turtle.PenSize = 3;
+            Turtle.MoveTo(-400, -150);
+            Turtle.RotateTo(25);
+            Turtle.PenUp();
+
+
+            // initializes and runs the l-system
+            system lsys = new system();
+            lsys.next_gen(depth,"X");
+
+
+            for (int i = 0; i < depth; i++) //runs for each generation of the tree. This is not striktly neccissary,
+                                            //but is fairly unintensive, and simply coppies over. It can be used to alter the 
+                                            //width of the branches at higher levels.
+            {
+                              
+                Stack<int[]> stk = new Stack<int[]>(); //creates the stack for generation at runtime
+
+                this_line = lsys.gen_tree.Pop();//pops the oldest generation from the stack of generationscerated
+                                                //by the l-system
+
+
+                //resets the tuertle at the base of the tree and at the correct angle
+                Turtle.PenDown();                
+                Turtle.RotateTo(45);
+                Turtle.PenUp();
+                Turtle.MoveTo(-600, -300);
+                Turtle.PenDown();
+
+
+                //generates the tree based on the characters in the gurrent generation ruleset
+                foreach (char c in this_line)
+                {
+                   //values of the turtle if needed to be pushed to the stack
+                    int[] to_place = new int[3];
+                    to_place[0] = Convert.ToInt16(Turtle.X);
+                    to_place[1] = Convert.ToInt16(Turtle.Y);
+                    to_place[2] = Convert.ToInt16(Turtle.Angle);
+
+                    //the rules depending on character
+                    if (c == 'F') {
+                        Turtle.Forward(length); }
+
+                    if (c== 'X') {//draws leaf nodes
+                        Turtle.PenColor = Color.Green;
+                        
+                        Turtle.Forward(length*2);
+                        
+                        Turtle.PenColor = Color.Brown;
+                    }
+
+                    if (c == '-') {//rotates negatiely
+                        Turtle.Rotate(neg_angle); }
+
+                    if (c == '+') {//rotates positively
+                        Turtle.Rotate(pos_angle); }
+
+                    if (c == '[')
+                    {//pushes to stack
+                        stk.Push(to_place);
+                    }
+                    
+                    if (c == ']')
+                    {//pops from stack, and updates turtle position
+                        Turtle.PenUp();
+                        Turtle.MoveTo(stk.Peek()[0], stk.Peek()[1]);
+                        Turtle.RotateTo(stk.Pop()[2]);
+                        Turtle.PenDown();
+                    }
+                }
+
+            }
+
+        }
+
+        private void Clear_btn_Click(object sender, EventArgs e)
+        {
+            Turtle.PenColor = Color.White;
+            Turtle.PenSize = 2000;
+            Turtle.PenDown();
+            Turtle.MoveTo(0, 0);
+        }
+
+        private void posang_txtb_TextChanged(object sender, EventArgs e)
+        {
+            posang = posang_txtb.Text;
+        }
+
+        private void negang_txtb_TextChanged(object sender, EventArgs e)
+        {
+            negang = negang_txtb.Text;
+        }
+
+        private void Depth_bar_Scroll(object sender, EventArgs e)
+        {
+            depth = Depth_bar.Value;
+            depth_lbl.Text = "Depth = " + Depth_bar.Value;
+        }
+
+        private void Length_txtb_TextChanged(object sender, EventArgs e)
+        {
+            if (Length_txtb.Text != "")
+            {
+            length = Convert.ToInt32(Length_txtb.Text);
+            }
+
+        }
+    }
+
+
+    class system
+    {
+        //creates dictionary for rules
+        Dictionary<char, string> rules = new Dictionary<char, string>();
+
+        //creates key value pairs on initalization
+        public system( )
+        {
+            rules.Add('F', "FF");
+            rules.Add('X', "F-[[X]+X]+F[+FX]-X");
+            //rules.Add('X', "F[++F[X]]-F+X");
+        }
+
+        public void clear()
+        {
+            current = "";
+        }
+
+        //creates stack of generations
+        public Stack<string> gen_tree = new Stack<string>();
+
+        //uses the rules dictionary to proccess the current gerneration and push the nex
+        //generation to the stack.
+        public string current;
+        public void next_gen(int gen, string axiom)
+        {
+            if (gen != 0)
+            { 
+
+                string new_str = "";
+                foreach (char c in axiom)
+                {
+                    //writes with new rule if needed, and copies if not
+                    if (rules.ContainsKey(c))
+                    {
+                        new_str += rules[c];
+                    }
+                    else
+                    {
+                        new_str += c;
+                    }
+                }
+                current = new_str;
+                gen_tree.Push(current);
+                next_gen(gen - 1,current);
+            }
+        }
+    }
+}
